5ff4424ad12a18e764da10259d501603ca8d8415
Sourabh Vamdevan <vamdevansourabh@gmail.com>
2025-11-02 09:54:02 +0530

    new changes

---
+++ lib/db.ts
@@ -0,0 +1,14 @@
+
+import { PrismaClient } from '@prisma/client';
+declare global{
+
+    var prisma: PrismaClient | undefined;
+
+}
+
+export const db =globalThis.prisma || new PrismaClient();
+globalThis.prisma=db;
+
+
+
+
